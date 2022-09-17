721c229977acdcae0be02c73157d8517cd1fa2f8
Puneet Dwivedi <pdwivedi294@gmail.com>
2022-09-17 12:23:43 +0530

    version 1

--- src/api/controllers/userAuth.js
+++ src/api/controllers/userAuth.js
@@ -23,7 +23,7 @@ const userAuth = async (req, res) => {
             }
         }
     } catch (err) {
-        res.status(500).json({ success: "ok", result: err });
+        res.status(500).json({ success: "fail", result: err });
     }
 }
 
